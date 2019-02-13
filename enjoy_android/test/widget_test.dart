4d135a919be1d68cc6f1939709fb8b4abead78dd
halewang <wangh@tttell.com>
2019-02-13 23:28:26 +0800

    更改入口文件路径

--- enjoy_android/test/widget_test.dart
+++ enjoy_android/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:enjoy_android/main/main.dart';
+import 'package:enjoy_android/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
