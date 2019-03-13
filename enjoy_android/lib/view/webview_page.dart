893785092af4804561a9482ae18d109f9a7d2bc8
wanghao260 <wanghao60@jd.com>
2019-03-13 11:01:36 +0800

    去掉无用代码

--- enjoy_android/lib/view/webview_page.dart
+++ enjoy_android/lib/view/webview_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class WebViewPage extends StatefulWidget {
@@ -19,33 +18,10 @@ class WebViewPage extends StatefulWidget {
 
 class _WebViewState extends State<WebViewPage>{
 
-  FlutterWebviewPlugin flutterWebViewPlugin = new FlutterWebviewPlugin();
-  StreamSubscription<WebViewStateChanged> _onStateChanged;
-
   @override
   void initState() {
     super.initState();
-    flutterWebViewPlugin.close();
-    _onStateChanged =
-        flutterWebViewPlugin.onStateChanged.listen((WebViewStateChanged state) {
-          switch (state.type) {
-            case WebViewState.shouldStart:
-            // 准备加载
-              print("准备加载");
-              break;
-            case WebViewState.startLoad:
-            // 开始加载
-              print("开始加载");
-              break;
-            case WebViewState.finishLoad:
-            // 加载完成
-              print("加载完成");
-              break;
-          }
-        });
-    flutterWebViewPlugin.onDestroy.listen((_) {
-      Navigator.of(context).pop();
-    });
+
   }
 
   @override
